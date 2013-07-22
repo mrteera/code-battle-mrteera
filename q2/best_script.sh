73420706ab2ffee066719d5bab9935e1c9c7d2e1
MrTeera <webmaster@mrteera.com>
2013-07-23 03:14:41 +0700

    merge q2 awk string

--- q2/best_script.sh
+++ q2/best_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nice -n 00 awk '{count[$1]++}END{for(c in count) print "\x22"c"\x22,", ""count[c]""}' numbers.txt > run_result.txt
+nice -n 00 awk '{count[$1]++}END{for(c in count) print "\x22"c"\x22, "count[c]""}' numbers.txt > run_result.txt
