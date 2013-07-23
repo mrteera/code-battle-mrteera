db0d9d6a5dc647f918821f87fffae4b18f72c43e
MrTeera <webmaster@mrteera.com>
2013-07-23 15:11:08 +0700

    change nawk to mawk 1.3.4

--- q2/best_script.sh
+++ q2/best_script.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-nice -n 00 nawk '{count[$1]++}END{for(c in count) print "\x22"c"\x22, "count[c]""}' numbers.txt > run_result.txt
+nice -n 00 mawk '{count[$1]++}END{for(c in count) print "\x22"c"\x22, "count[c]""}' numbers.txt > run_result.txt
